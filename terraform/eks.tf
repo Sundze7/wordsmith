9e2917b984ffca8f7e72c1c518005cb45bf23a0c
Sundze <keyonmwesundze@mail.com>
2024-06-26 17:17:48 +0100

    modifications

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -1,4 +1,3 @@
-#eks-cluster.tf
 module "eks" {
     source  = "terraform-aws-modules/eks/aws"
     version = "~> 19.0"
@@ -7,13 +6,12 @@ module "eks" {
 
     cluster_endpoint_public_access  = true
 
-    vpc_id = "vpc-02fa0c5292a4b44bf"
-    subnet_ids = module.myjenkins-server-vpc.public_subnets
-    # subnet_ids = "subnet-04158b6f5c1547e2a"
+    vpc_id = module.myjenkins-server-vpc.vpc_id
+    subnet_ids = module.myjenkins-server-vpc.private_subnets
 
     tags = {
         environment = "development"
-        application = "eks-server"
+        application = "myjenkins-server"
     }
 
     eks_managed_node_groups = {
@@ -26,3 +24,32 @@ module "eks" {
         }
     }
 }
+
+# #eks-cluster.tf
+# module "eks" {
+#     source  = "terraform-aws-modules/eks/aws"
+#     version = "~> 19.0"
+#     cluster_name = "myjenkins-server-eks-cluster"
+#     cluster_version = "1.24"
+
+#     cluster_endpoint_public_access  = true
+
+#     vpc_id = "vpc-02fa0c5292a4b44bf"
+#     subnet_ids = module.myjenkins-server-vpc.public_subnets
+#     # subnet_ids = "subnet-04158b6f5c1547e2a"
+
+#     tags = {
+#         environment = "development"
+#         application = "eks-server"
+#     }
+
+#     eks_managed_node_groups = {
+#         dev = {
+#             min_size = 1
+#             max_size = 3
+#             desired_size = 2
+
+#             instance_types = ["t2.small"]
+#         }
+#     }
+# }
--- terraform/terraform.tfvars
+++ terraform/terraform.tfvars
@@ -1,3 +1,7 @@
-vpc_cidr_block = "172.31.0.0/16"
-# private_subnet_cidr_blocks=["10.0.1.0/24","10.0.2.0/24","10.0.3.0/24"]
-public_subnet_cidr_blocks=["172.31.32.0/20","172.31.16.0/20","172.31.0.0/20"]
+vpc_cidr_block = "10.0.0.0/16"
+private_subnet_cidr_blocks=["10.0.1.0/24","10.0.2.0/24","10.0.3.0/24"]
+public_subnet_cidr_blocks=["10.0.4.0/24","10.0.5.0/24","10.0.6.0/24"]
+
+# vpc_cidr_block = "172.31.0.0/16"
+# # private_subnet_cidr_blocks=["10.0.1.0/24","10.0.2.0/24","10.0.3.0/24"]
+# public_subnet_cidr_blocks=["172.31.32.0/20","172.31.16.0/20","172.31.0.0/20"]
